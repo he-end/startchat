f92f47ac4bdda26fc0fbf277171981f1ddbb60c0
endbot <hendri41234@gmail.com>
2025-07-05 02:06:47 +0700

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"sc/internal/repository/repootp"
+	"sc/internal/logger"
+	serviceotp "sc/internal/service/otp"
 )
 
 func main() {
-	x := repootp.NewOTP("hendri@email.com")
-	fmt.Println(x)
+	logger.Init(logger.Config{
+		Environment:     "production",
+		LogToConsole:    true,
+		LogToFile:       true,
+		LogToRemote:     false,
+		EnableRolling:   true, // rolling log aktif
+		LogFilePath:     "logs/app.log",
+		MinimumLogLevel: "debug",
+	})
+	defer logger.Log.Sync()
+
+	// mail := "test123@test.com"
+	// fmt.Println("=> generate new otp")
+	// newOtp, _ := repootp.NewOTP(mail)
+	// if len(newOtp.Email) < 1 {
+	// 	log.Fatal()
+	// }
+	// fmt.Println("=> wait for a second")
+	// time.Sleep(time.Second * 2)
+	// fmt.Println("=> test matching")
+	// ok, err := serviceotp.VerifyOtp(mail, newOtp.OTPCode)
+	// if !ok {
+	// 	if err != nil {
+	// 		logger.Error("error mathing otp", zap.Error(err))
+	// 		return
+	// 	}
+	// 	logger.Error("otp not match", zap.String("otp", newOtp.OTPCode))
+	// 	return
+	// }
+	// fmt.Println("otp match")
+
+	serviceotp.SendOTPWithGmail("1010101", "hendri41234@gmail.com")
 }
