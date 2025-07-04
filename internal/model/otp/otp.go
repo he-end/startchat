00b6aa81b83a61da8f0f5ea7217b1d1f0f9a0448
endbot <hendri41234@gmail.com>
2025-07-05 00:24:50 +0700

    add ID field in OTP models

--- internal/model/otp/otp.go
+++ internal/model/otp/otp.go
@@ -1,15 +1,19 @@
 package modelotp
 
-import "time"
+import (
+	"sc/internal/repository"
+	"time"
+)
 
 type OTP struct {
-	Email     string    `json:"email"`
-	Phone     string    `json:"phone"`
-	OtpCode   string    `json:"otp_code"`
-	ExpiresAt time.Time `json:"expires_at"`
-	Verified  bool      `json:"verified"`
-	CratedAt  time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string                `json:"id"`
+	Email     repository.NullString `json:"email"`
+	Phone     repository.NullString `json:"phone"`
+	OtpCode   string                `json:"otp_code"`
+	ExpiresAt time.Time             `json:"expires_at"`
+	Verified  bool                  `json:"verified"`
+	CratedAt  time.Time             `json:"created_at"`
+	UpdatedAt time.Time             `json:"updated_at"`
 }
 
 type ReqOTP struct {
