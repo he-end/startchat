2b2bf578c30d52146bace9d2d799cfcb0f11201e
endbot <hendri41234@gmail.com>
2025-07-05 21:00:05 +0700

    update

--- internal/service/user/new_register.go
+++ internal/service/user/new_register.go
@@ -7,9 +7,9 @@ import (
 
 func NewRegister(email, password, ipAddress string) (tokenRegister string, err error) {
 	// otp, err = repootp.NewOTP(email)
-	if err != nil {
-		return
-	}
+	// if err != nil {
+	// 	return
+	// }
 
 	token, err := authrandtoken.GenerateSecureRandomToken()
 	if err != nil {
